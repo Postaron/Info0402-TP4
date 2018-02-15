fa7f565583460cee4fcfd3ad63204d498a876621
Postaron <florent.denef@gmail.com>
2018-02-15 09:29:40 +0100

    Interface ajoutée.

---
+++ Transform.h
@@ -0,0 +1,12 @@
+#ifndef TRANSFORM_H_
+#define TRANSFORM_H_
+
+class Transform {
+public:
+	virtual void Rotate() = 0;
+	virtual void Translate() = 0;
+	virtual void Scale() = 0;
+	virtual ~Transform() = 0;
+};
+
+#endif /* TRANSFORM_H_ */
